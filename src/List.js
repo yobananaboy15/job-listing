e0c9bff593938165f0be8776935692fdaa653177
Axel Thiel <axth2220@gmail.com>
2020-12-04 11:12:07 +0100

    clarified comment about filterList

--- src/List.js
+++ src/List.js
@@ -4,7 +4,7 @@ import ListItem from "./ListItem";
 const List = ({ jobData, filter, addFilter }) => {
   //Loopa igenom filter och kolla i motsvarande arrays.
 
-  //Filters the jobData array by checking if the job objects contain everything in the filter set.
+  //Filters the jobData array by checking if the every job object contain everything in the filter set.
   const filterList = jobData.filter((job) => {
     const categoryArray = [job.role, job.level, ...job.tools, ...job.languages];
     return [...filter].every((categoryItem) =>
