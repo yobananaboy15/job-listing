61c768f612f9ba085c4ad37025e3c99d7126e24e
Axel Thiel <axth2220@gmail.com>
2020-12-04 09:30:38 +0100

    changed name of filterbar to filter

--- src/Filter.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Filter = () => {
-  return <div>filter</div>;
-};
-
-export default Filter;
---
+++ src/Filterbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RemoveFilterButton from "./RemoveFilterButton";
+
+const Filterbar = () => {
+  return (
+    <div>
+      <img src="./images/icon-remove.svg" alt="" />
+    </div>
+  );
+};
+
+export default Filterbar;
