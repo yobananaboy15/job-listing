bf1d429a8c68f188afbfa3d4862e50bc06c4834f
Axel Thiel <axth2220@gmail.com>
2020-12-09 12:45:08 +0100

    fixed image import

--- src/RemoveFilterButton.js
+++ src/RemoveFilterButton.js
@@ -1,16 +1,20 @@
 import React from "react";
+import styles from "./RemoveFilterButton.module.css";
+import crossIcon from "../src/images/icon-remove.svg";
 
 export default function RemoveFilterButton({ filterItem, removeFilter }) {
   return (
-    <>
-      <span>
-        {filterItem}
-        <img
-          src="./images/icon-remove.svg"
-          alt=""
-          onClick={() => removeFilter(filterItem)}
-        />
-      </span>
-    </>
+    <div className={styles["category-span"]}>
+      <div>{filterItem}</div>
+      <div className={styles["button-container"]}>
+        <button className={styles["remove-icon"]}>
+          <img
+            src={crossIcon}
+            alt=""
+            onClick={() => removeFilter(filterItem)}
+          />
+        </button>
+      </div>
+    </div>
   );
 }
